f834fc5aa5dc91068006d176d65c7b7f6012c85b
EliezerRubin19 <43821528+EliezerRubin19@users.noreply.github.com>
2018-10-17 16:32:14 +0300

    Ex1

---
+++ Strings/Exercise1.java
@@ -0,0 +1,21 @@
+
+public class Exercise1 {
+
+	 
+	
+	  public static void main(String args[]) {
+
+	    String s = "abcdefghijklmnopqrstuvwxy";
+	    int start = 0;
+	    int end = 5;
+	    while (end<=s.length()){
+	    char buf[] = new char[end - start];
+
+	    s.getChars(start, end, buf, 0);
+	    System.out.println(buf);
+	    start+=5;
+	    end+=5;
+	  }
+	}
+}
+
