aee8fde5b749ec97e914eb1212e4792f884e78f9
EliezerRubin19 <43821528+EliezerRubin19@users.noreply.github.com>
2018-10-24 16:45:48 +0300

    java

--- Strings/Exercise2.java
+++ Strings/Exercise2.java
@@ -1,25 +1,33 @@
 
+import java.util.Scanner;
+
 public class Exercise2 {
 
 	
 	public static void main(String args[]) {
-		
-		String s1 = "the quick brown fox";
-		String s2 = "queen";
+		 
+		Scanner scan=new Scanner(System.in);		
+		String s1 = scan.nextLine();
+		String s2 = scan.nextLine() ;
 		
 		char[] char1 =  s1.toCharArray();
 		char[] char2 =  s2.toCharArray();
 		
-		
-		for (int i = 0; i < char2.length; i++) {
-			for (int j = 0; j < char1.length; j++) {
-				if (char1[j]==char2[i]) char1[j]=8;
-			}
-		}
+		String s="";
+		int k=0;
 		for (int i = 0; i < char1.length; i++) {
-			if (char1[i]!=8)
-			System.out.print(char1[i]);
+			for (int j = 0; j < char2.length; j++) {
+				if (char1[i]==char2[j])k=0;else k++;
+			}
+				if (k==char2.length) s+=char1[i];
+				k=0;
+			}
+		System.out.println(s);
+		
+		scan.close();
+		}
+		
 		}
 			
-	}
-}
+	
+
